7b03c0629d2c2afe99bde95e30401a1960e25497
Suntx1994 <tianxiang.sun@hotmail.com>
2019-11-13 16:48:06 -0500

    changeunittest

--- MovieShop.UnitTest/UnitTest1.cs
+++ MovieShop.UnitTest/UnitTest1.cs
@@ -10,5 +10,26 @@ namespace MovieShop.UnitTest
         {
             Assert.AreEqual(1, 1);
         }
+        [TestMethod]
+        public void TestFakeData2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestFakeData3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestFakeData4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestFakeData5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
     }
 }
