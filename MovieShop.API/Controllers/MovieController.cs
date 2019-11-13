859318da10166b2363e0692340231600163fc3b8
Suntx1994 <tianxiang.sun@hotmail.com>
2019-11-13 16:03:50 -0500

    changedfirstcommit

--- MovieShop.API/Controllers/MovieController.cs
+++ MovieShop.API/Controllers/MovieController.cs
@@ -27,5 +27,13 @@ namespace MovieShop.API.Controllers
             return Ok(movies);
         }
 
+
+        [HttpGet]
+        [Route("topgrossing2")]
+        public async Task<IActionResult> GetTopGrossingMovies2()
+        {
+            var movies = await _movieService.GetHighestGrossingMovies();
+            return Ok(movies);
+        }
     }
 }
---
+++ MovieShop.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieShop.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFakeData1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
